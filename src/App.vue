b61243d0f36cf9e3e46f98021f4d4ae3b351c5ae
_ <youngdelta@gmail.com>
2023-01-07 17:28:32 +0900

    c1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h2 class="page-subtitle">MBTI</h2>
+	<h1 class="page-title">
+		내 안에 숨어있는<br />
+		직업캐 찾기!
+	</h1>
+	<img
+		class="character"
+		src="/images/main_character.png"
+		alt="메인 이미지" />
+	<a
+		href="/questions.html"
+		class="btn btn-orange"
+		>본캐 찾으러 GO!</a
+	>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	// import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'App',
+		components: {},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	/* #app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	} */
 </style>
